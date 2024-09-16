77fffea7476595fbe95b9b56b061f9b25721385a
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-09-16 18:38:02 +0400

    Add logic for age and gender validation and selection visuals

--- scripts/age.js
+++ scripts/age.js
@@ -1,10 +1,31 @@
 const backButton = document.querySelector("#back-button");
 const confirmButton = document.querySelector("#confirm-button");
+const optionDivs = document.querySelectorAll(".select-div>div");
+let selectedAge;
 
-backButton.addEventListener("click", (e) => {
-    window.location.href = "../pages/gender.html";
+optionDivs.forEach((option) => {
+    option.addEventListener("click", (e) => {
+        disableAllDivs();
+        option.querySelector("img").classList.add("selected");
+        selectedAge = option.dataset.age;
+    });
 });
 
 confirmButton.addEventListener("click", (e) => {
-    window.location.href = "../pages/how-it-works.html";
+    if (selectedAge) {
+        window.localStorage.setItem("age", selectedAge);
+        window.location.href = "../pages/how-it-works.html";
+    } else {
+        alert("Please, Select an option."); //add visuals to let user know to pick an option
+    }
 });
+
+backButton.addEventListener("click", (e) => {
+    window.location.href = "../pages/gender.html"
+});
+
+function disableAllDivs() {
+    optionDivs.forEach((option) => {
+        option.querySelector("img").classList.remove("selected");
+    });
+}
--- scripts/gender.js
+++ scripts/gender.js
@@ -1,5 +1,26 @@
 const backButton = document.querySelector("button");
+const optionDivs = document.querySelectorAll(".select-div>div");
+let selectedGender;
+
+optionDivs.forEach((option) => {
+    option.addEventListener("click", (e) => {
+        disableAllDivs();
+        option.querySelector("img").classList.add("selected");
+        selectedGender = option.dataset.gender;
+    });
+});
 
 backButton.addEventListener("click", (e) => {
-    window.location.href = "../pages/age.html";
+    if (selectedGender) {
+        window.localStorage.setItem("gender", selectedGender);
+        window.location.href = "../pages/age.html";
+    } else {
+        alert("Please, Select an option."); //add visuals to let user know to pick an option
+    }
 });
+
+function disableAllDivs() {
+    optionDivs.forEach((option) => {
+        option.querySelector("img").classList.remove("selected");
+    });
+}
